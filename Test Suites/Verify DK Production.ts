03a0ecf8e1707fed66a95cf4a24822d6ec487660
hassanhashem <hassan.hashem.74@gmail.com>
2017-05-15 09:11:02 +0200

    hh

--- Test Suites/Verify DK Production.ts
+++ Test Suites/Verify DK Production.ts
@@ -4,7 +4,7 @@
    <name>Verify DK Production</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2017-05-13T19:28:58</lastRun>
+   <lastRun>2017-05-13T22:46:38</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
